4d79d299e3e0c52ccdad7b0c7cc237411eb0babc
jmrivas <jmrivas86@gmail.com>
2022-05-04 00:51:00 +0200

    Delete unused lines

--- face_learning.py
+++ face_learning.py
@@ -17,13 +17,12 @@ for name_dir in people_list:
         print('Rostros: ', name_dir + '/' + file_name)
         labels.append(label)
         faces_data.append(cv2.imread(person_path + '/' + file_name, 0))
-        # image = cv2.imread(personPath+'/'+fileName,0)
-        # cv2.imshow('image',image)
-        # cv2.waitKey(10)
+
     label = label + 1
 print('labels= ', labels)
 print('Número de etiquetas 0: ', np.count_nonzero(np.array(labels) == 0))
 print('Número de etiquetas 1: ', np.count_nonzero(np.array(labels) == 1))
+
 # Entrenar el reconocedor
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
